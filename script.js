e8da83dd7a54bd0bdef30417b0733023f1a9df45
Lolotte31 <laurenzolivier@gmail.com>
2018-12-13 21:25:54 +0100

    ajout du script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// pop up au bout de 10s
+setTimeout(
+	function() {
+		var coquin = confirm("Hey le site xxxvidsxxx est trop bien. Tu viens le visiter?");
+    		if ( coquin ) {
+      			window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+       				 		 } 
+       			},10000);
+
+
+
+     
